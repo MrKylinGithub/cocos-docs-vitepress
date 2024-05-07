ed052e2776979a740c24a79e3e802cab68d6847b
alan <741484865@qq.com>
2024-05-07 15:31:28 +0800

    feat: publish

--- versions/3.8/.vitepress/config/index.ts
+++ versions/3.8/.vitepress/config/index.ts
@@ -8,7 +8,7 @@ import { version } from './version.ts';
 
 export default defineConfig({
   ...shared,
-  base: `/${version}/`,
+  base: `/`, // `/${version}/`
   locales: {
     // 我们的文档都在 /en/ /zh/ 等文件夹里面，没有在根目录的文档 所以不用配 root
     // root: { label: 'English', ...en },
