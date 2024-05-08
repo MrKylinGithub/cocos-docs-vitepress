5d29f1c8dbb28c133483a208d05aca9e360c9343
alan <741484865@qq.com>
2024-05-08 09:59:16 +0800

    feat: 整合多语言配置

--- @config/shared-theme-en.ts
+++ @config/shared-theme-en.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vitepress'
 import { github } from './config'
 
-export default defineConfig({
-  lang: 'en-US',
-  description: 'Vite & Vue powered static site generator.',
-
-  themeConfig: {
-    
-    editLink: {
-      pattern: `${github}/edit/master/:path`,
-      text: 'Edit this page on GitHub'
-    },
-
-    footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2019-present Evan You'
+export default function(version: string) {
+  return defineConfig({
+    lang: 'en-US',
+    description: 'Vite & Vue powered static site generator.',
+  
+    themeConfig: {
+      
+      editLink: {
+        pattern: `${github}/edit/master/versions/${version}/:path`,
+        text: 'Edit this page on GitHub'
+      },
+  
+      footer: {
+        message: 'Released under the MIT License.',
+        copyright: 'Copyright © 2019-present Evan You'
+      }
     }
-  }
-})
+  })
+}
--- versions/3.8/.vitepress/config/index.ts
+++ versions/3.8/.vitepress/config/index.ts
@@ -1,10 +1,50 @@
 import { defineConfig } from 'vitepress'
 import { shared } from '../../../../@config/shared'
-import { en } from './en'
-import { zh } from './zh'
+
+// 每个语言的配置
+import configZhFn from '../../../../@config/shared-theme-zh'
+import configEnFn from '../../../../@config/shared-theme-en'
+import { nav } from '../../../../@config/shared';
+
+// 侧边栏数据
+import menusEN from '../../en/summary.json' with {type: 'json'};
+import menusZH from '../../zh/summary.json' with {type: 'json'};
+
 import { version } from './version.ts';
 
+// 英文配置
+const configEn = configEnFn(version);
+const en = defineConfig({
+  lang: configEn.lang,
+  description: configEn.description,
 
+  themeConfig: {
+    ...configEn.themeConfig,
+
+    nav: nav(version, 'en'),
+
+    sidebar: {
+      '/en/': { base: '/en/', items: menusEN },
+    }
+  }
+})
+
+// 中文配置
+const configZh = configZhFn(version);
+const zh = defineConfig({
+  lang: configZh.lang,
+  description: configZh.description,
+
+  themeConfig: {
+    ...configZh.themeConfig,
+    
+    nav: nav(version, 'zh'),
+
+    sidebar: {
+      '/zh/': { base: '/zh/', items: menusZH },
+    }
+  }
+})
 
 export default defineConfig({
   ...shared,
