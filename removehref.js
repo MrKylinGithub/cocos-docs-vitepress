d007fc1272da61f29c8af15c4afdd371ad39706d
Mr. Kylin <738337822@qq.com>
2024-05-20 14:00:59 +0800

    Update removehref.js

--- removehref.js
+++ removehref.js
@@ -49,7 +49,7 @@ function traverseDirectory(directoryPath) {
 }
 
 // 指定要遍历的文件夹路径
-const directoryPath = path.join('./', './versions/3.6');
+const directoryPath = path.join('./', './versions/3.8');
 
 // 调用函数遍历指定文件夹
 traverseDirectory(directoryPath);
